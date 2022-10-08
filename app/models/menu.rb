f4b3517afd9ff4b51e7e5ac5a1c22659fa4771be
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-10-08 01:54:40 +0000

    モデルの関連性設定

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,13 @@
 class Menu < ApplicationRecord
   has_one_attached :image
   
+  belongs_to :user
+  
+  has_many :favorites ,dependent: :destroy
+  has_many :comments ,dependent: :destroy
+  
   # 中間テーブル
   has_many :menugenres
   has_many :genres, through: :menugenres
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  has_many :menus
+  has_many :favorites
+  has_many :comments
+
 end
