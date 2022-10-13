de6b7af1a9a16984842df49f1c630459938eaeeb
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-10-13 12:11:04 +0000

    いいねindexの調整

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,21 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
-    menu = Menu.find(params[:menu_id])
+    @menu = Menu.find(params[:menu_id])
     @comment = current_user.comments.new(comment_params)
-    @comment.menu = menu
+    @comment.menu = @menu
     @comment.user_id = current_user.id
     @comment.save
-    # redirect_to menu_path(menu)
   end
 
   def destroy
-    # binding.pry
-    menu = Menu.find(params[:menu_id])
-    @comment = menu.comments.find(params[:id])  #1対多の関係で記載
-    # @comment.menu = menu
-    # @comment.user_id = current_user.id
+    @menu = Menu.find(params[:menu_id])
+    @comment = @menu.comments.find(params[:id])  #1対多の関係で記載
     @comment.destroy
-    # redirect_to menu_path(menu)
   end
 
 
--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,7 +3,8 @@ class Public::FavoritesController < ApplicationController
   before_action :set_menu, except: [:index] #exceptは覗く時に活用
   before_action :authenticate_user!   # ログイン中のユーザーのみに許可（未ログインなら、ログイン画面へ移動）
   def index
-    @favorites = Favorite.all.page(params[:page]).per(12)
+    user = current_user
+    @favorites = user.favorites.page(params[:page]).per(12)
   end
 
   # お気に入り登録
