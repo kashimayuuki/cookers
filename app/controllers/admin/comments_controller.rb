f859362fabe6ffe003beb67981b812024eea413b
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2022-10-17 11:45:29 +0000

    管理者　レイアウト修正

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,15 +1,14 @@
 class Admin::CommentsController < ApplicationController
   def index
-    @comments = Comment.all
   end
 
   def show
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    user = User.find(params[:user_id])
+    @user = User.find(params[:id])
+    @comment = @user.comments
     @comment.destroy
-    redirect_to admin_user_path(user)
+    redirect_to admin_user_path(@user)
   end
 end
--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -2,4 +2,10 @@ class Admin::MenusController < ApplicationController
   def show
     @menu = Menu.find(params[:id])
   end
+
+  def destroy
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    redirect_to admin_root_path
+  end
 end
